fb6849b5fc63b63c72e509e21d42bb43431dd0a3
Sroy86 <54235302+Sroy86@users.noreply.github.com>
2020-07-29 16:09:48 +0530

    react  unorder list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-ReactDOM.render(<h1>Hello World!</h1>,document.getElementById("root"));
- 
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favrourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamaon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
